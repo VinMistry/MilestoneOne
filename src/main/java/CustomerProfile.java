b41212fdff09f0dcaf9ee7a38a066309b05e1807
Vinesh Mistry <vinesh.mistry@moneysupermarket.com>
2018-09-17 21:06:02 +0100

    More refactoring

--- src/main/java/CustomerProfile.java
+++ src/main/java/CustomerProfile.java
@@ -1,12 +1,7 @@
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class CustomerProfile {
 
   private Customer customer;
   private Car car;
-  private FilePersistor filePersistor;
 
   public CustomerProfile() {
     customer = new Customer();
@@ -34,34 +29,9 @@ public class CustomerProfile {
     this.car = car;
   }
 
-  private FilePersistor getFilePersistor() {
-    return filePersistor;
-  }
-
-  public void setFilePersistor(final FilePersistor filePersistor) {
-    this.filePersistor = filePersistor;
-  }
-
-  public void printCustomerDetails() {
-    System.out.println(customer.toString());
-
-    System.out.println(car.toString() + "\n");
-  }
-
-  public void writeSingleProfileToTextFile(final String name) throws IOException {
-    final String str = customer.toString() + "\n" + car.toString();
-    final byte[] strToBytes = str.getBytes();
-    final Path path = Paths.get("");
-    final String dirPath = path.toAbsolutePath().toString();
-    final Path currentDirPath = Paths.get(dirPath + "/" + name + ".txt");
-    getFilePersistor().persistData(currentDirPath, strToBytes);
-  }
-
   @Override
   public String toString() {
     return "Customer Profile: \n" + customer.toString() + "\n" + car.toString();
   }
 
-  public static void main(final String[] args) {
-  }
 }
